ed681f4a585f4eb54ef7c9fddba0bf6198a4c3df
kunimisun <83684034+kunimisun@users.noreply.github.com>
2022-02-14 09:39:21 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+
+    function callback(entries,obs){
+        entries.forEach(entry => {
+            
+            
+        });
+    }
+    const observer =new IntersectionObserver(callback,options);
+}
